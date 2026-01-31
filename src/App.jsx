356d300da1284edaa99b8a18527eb12948defb61
Rishabh Garg <rishabhgarg4985@gmail.com>
2026-01-31 09:34:57 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar
+        brand="Biggies Corporation"
+        links={["Home", "About Us", "Contact"]}
+      />
       <HomePage />
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Counter from "../components/Counter";
-import Navbar from "../components/Navbar";
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar links={["Home", "About Us"]} brand="Biggies Corporations" />
       <Counter />
     </div>
   );
